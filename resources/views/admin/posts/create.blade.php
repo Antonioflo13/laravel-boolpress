985950d8352873fb7294b349da8b483942c74264
Antonioflo13 <floreantonio@hotmail.it>
2021-07-27 17:57:47 +0200

    add: error view

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,18 +8,24 @@
         @method('POST')
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" placeholder="Inserisci il titolo" name="title" value="{{ old("title") }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror"" id="title" placeholder="Inserisci il titolo" name="title" value="{{ old("title") }}">
+            @error('title')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Contenuto</label>
-            <input type="text" class="form-control" id="series" placeholder="Inserisci il contenuto" name="content" value="{{ old("content") }}">
+            <input type="text" class="form-control @error('content') is-invalid @enderror"" id="content" placeholder="Inserisci il contenuto" name="content" value="{{ old("content") }}">
+            @error('content')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="category_id" class="form-label">Categoria</label>
             <select class="form-control" name="category_id" id="category_id">
                 <option for="category_id" class="form-label" disabled selected>-- Seleziona la Categoria --</option>
                 @foreach ($categories as $category)
-                <option value="{{ $category->id }}" {{ $category->id == old('cateogy_id') ? 'selected' : '' }}>{{$category->name}}</option>
+                <option value="{{ $category->id }}" {{ $category->id == old('category_id') ? 'selected' : '' }}>{{$category->name}}</option>
                 @endforeach
             </select>
         </div>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,11 +8,17 @@
         @method('PATCH')
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" placeholder="Inserisci il titolo" name="title" value="{{ old("title",$post->title) }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror"" id="title" placeholder="Inserisci il titolo" name="title" value="{{ old("title",$post->title) }}">
+            @error('title')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Contenuto</label>
-            <input type="text" class="form-control" id="series" placeholder="Inserisci il contenuto" name="content" value="{{ old("content",$post->content) }}">
+            <input type="text" class="form-control @error('content') is-invalid @enderror"" id="series" placeholder="Inserisci il contenuto" name="content" value="{{ old("content",$post->content) }}">
+            @error('content')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="category_id" class="form-label">Categoria</label>
